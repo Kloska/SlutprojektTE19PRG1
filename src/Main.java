4f94eaa7ae7baf37940e9283ae7de78d37ca88de
Kloska <56342396+Kloska@users.noreply.github.com>
2021-05-07 11:20:18 +0200

    Början

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created 2021-04-27
  *
@@ -5,6 +8,12 @@
  */
 public class Main {
     public static void main(String[] args) {
-        // Här startas ditt program
+        JFrame frame = new JFrame("Tegel Clicker");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        JButton Button = new JButton("Click");
+        Button.setBounds(50,100,95,30);
     }
+
 }
